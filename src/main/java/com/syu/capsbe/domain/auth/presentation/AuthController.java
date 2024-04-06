38aed5f005f15d7c42237f9c0e97ceab9047832e
kimnamgyu <lethargic.hood@gmail.com>
2024-04-06 23:03:41 +0900

    docs #11: 에러 코드 swagger 추가

--- src/main/java/com/syu/capsbe/domain/auth/presentation/AuthController.java
+++ src/main/java/com/syu/capsbe/domain/auth/presentation/AuthController.java
@@ -7,6 +7,7 @@ import com.syu.capsbe.domain.auth.dto.response.SignInResponseDto;
 import com.syu.capsbe.domain.auth.dto.response.SignUpResponseDto;
 import com.syu.capsbe.domain.member.Member;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -31,6 +32,9 @@ public class AuthController {
     @PostMapping("/sign-up")
     @Operation(summary = "회원 가입", description = "이메일을 통해 회원 가입 진행합니다.")
     @ApiResponse(responseCode = "201", description = "회원 가입 성공")
+    @ApiResponse(responseCode = "E0001", description = "이메일을 보내는 중 오류가 발생했습니다.", content = @Content)
+    @ApiResponse(responseCode = "E1001", description = "이메일 형식이 올바르지 않습니다.", content = @Content)
+    @ApiResponse(responseCode = "E1003", description = "이미 존재하는 회원입니다.", content = @Content)
     public SignUpResponseDto signUp(@RequestBody @Valid SignUpRequestDto signUpRequestDto) {
         return this.authService.signUp(signUpRequestDto);
     }
@@ -38,6 +42,7 @@ public class AuthController {
     @PostMapping("/sign-in")
     @Operation(summary = "로그인", description = "UUID를 통해 로그인을 진행합니다.")
     @ApiResponse(responseCode = "200", description = "로그인 성공")
+    @ApiResponse(responseCode = "E1002", description = "존재하지 않는 회원입니다.", content = @Content)
     public SignInResponseDto signIn(@RequestBody @Valid SignInRequestDto signInRequestDto) {
         return this.authService.signIn(signInRequestDto);
     }
