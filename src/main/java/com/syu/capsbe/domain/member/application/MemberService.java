45ff496322e9b42043d15a9c235120b5436b2be6
kimnamgyu <lethargic.hood@gmail.com>
2024-04-06 23:01:42 +0900

    feat #11: MemberService 구현 및 AuthService 작업 이관

---
+++ src/main/java/com/syu/capsbe/domain/member/application/MemberService.java
@@ -0,0 +1,14 @@
+package com.syu.capsbe.domain.member.application;
+
+import com.syu.capsbe.domain.member.Member;
+
+public interface MemberService {
+
+    Long save(Member member);
+
+    void checkMemberIsExist(String email);
+
+    Member findByUuid(String uuid);
+
+    Member findByEmail(String email);
+}
