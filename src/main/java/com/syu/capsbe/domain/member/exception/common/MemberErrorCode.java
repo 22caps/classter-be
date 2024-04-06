227b9085567184b3d1ea28e794b2df6775bb8819
kimnamgyu <lethargic.hood@gmail.com>
2024-04-06 23:05:39 +0900

    feat #11: 에러 코드 추가

--- src/main/java/com/syu/capsbe/domain/member/exception/common/MemberErrorCode.java
+++ src/main/java/com/syu/capsbe/domain/member/exception/common/MemberErrorCode.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MemberErrorCode {
 
-    INVALID_EMAIL_FORMAT("E1001", "이메일 형식이 올바르지 않습니다.");
+    INVALID_EMAIL_FORMAT("E1001", "이메일 형식이 올바르지 않습니다."),
+    MEMBER_IS_NOT_EXIST("E1002", "존재하지 않는 회원입니다."),
+    MEMBER_IS_ALREADY_EXIST("E1003", "이미 존재하는 회원입니다.");
 
     private final String errorCode;
     private final String errorMessage;
