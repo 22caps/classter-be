3295d7f55636294cbc972da124d9d21742a156af
kimnamgyu <lethargic.hood@gmail.com>
2024-04-06 23:04:03 +0900

    refactor #11: 에러 코드 명확하게 ( 0 -> 1 )

--- src/main/java/com/syu/capsbe/domain/auth/exception/common/AuthErrorCode.java
+++ src/main/java/com/syu/capsbe/domain/auth/exception/common/AuthErrorCode.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum AuthErrorCode {
 
-    EMAIL_SENDING_ERROR("E0000", "이메일을 보내는 중 오류가 발생했습니다.");
+    EMAIL_SENDING_ERROR("E0001", "이메일을 보내는 중 오류가 발생했습니다.");
 
     private final String errorCode;
     private final String errorMessage;
