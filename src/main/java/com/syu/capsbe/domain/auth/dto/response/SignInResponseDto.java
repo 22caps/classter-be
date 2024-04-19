0822feaf20802daf6c2981081e45b153c093bac2
kimnamgyu <lethargic.hood@gmail.com>
2024-04-19 23:05:29 +0900

    feat #35: 토큰 유효시간 추가

--- src/main/java/com/syu/capsbe/domain/auth/dto/response/SignInResponseDto.java
+++ src/main/java/com/syu/capsbe/domain/auth/dto/response/SignInResponseDto.java
@@ -1,6 +1,7 @@
 package com.syu.capsbe.domain.auth.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Date;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -13,7 +14,10 @@ public class SignInResponseDto {
     @Schema(description = "액세스 토큰", example = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0QHRlc3QuY29tIiwicm9sZXMiOlsiUk9MRV9VU0VSIl0sImlhdCI6MTcxMjIwOTc1MSwiZXhwIjoxNzE0ODAxNzUxfQ.RWRVAbvsM_Use6fwSGuMZ_FFa79xsGBrCwE-gYs3EGI")
     private final String accessToken;
 
-    public static SignInResponseDto of(String accessToken) {
-        return new SignInResponseDto(accessToken);
+    @Schema(description = "토큰 유효기간", example = "2024-04-19T14:32:37.248+00:00 / UTC 기준, KST 기준으로 +9시간 적용")
+    private final Date expiresAt;
+
+    public static SignInResponseDto of(String accessToken, Date expiresAt) {
+        return new SignInResponseDto(accessToken, expiresAt);
     }
 }
