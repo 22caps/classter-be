40e0d56f92e377e9cbb163aca1a6629d36f1f416
kimnamgyu <lethargic.hood@gmail.com>
2024-04-25 22:56:02 +0900

    docs #38: Problem 도메인 관련 swagger 문서화

--- src/main/java/com/syu/capsbe/domain/problem/dto/request/ProblemRequestDto.java
+++ src/main/java/com/syu/capsbe/domain/problem/dto/request/ProblemRequestDto.java
@@ -1,16 +1,20 @@
 package com.syu.capsbe.domain.problem.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
+@Schema(description = "문제 요청 정보")
 public class ProblemRequestDto {
 
     @NotNull
+    @Schema(description = "문제 유형", example = "WORD | CONVERSATION | GRAMMAR")
     private String problemType;
 
     @NotNull
+    @Schema(description = "문제 개수", example = "3 | 5 | 10")
     private int problemCount;
 }
