fb888093e00fa2a21246db53afd09a7689ae3cec
kimnamgyu <lethargic.hood@gmail.com>
2024-04-25 23:39:11 +0900

    feat #40: problemHint 메서드 구현

--- src/main/java/com/syu/capsbe/domain/problem/application/ProblemService.java
+++ src/main/java/com/syu/capsbe/domain/problem/application/ProblemService.java
@@ -1,10 +1,14 @@
 package com.syu.capsbe.domain.problem.application;
 
+import com.syu.capsbe.domain.problem.dto.request.ProblemHintRequestDto;
 import com.syu.capsbe.domain.problem.dto.request.ProblemRequestDto;
+import com.syu.capsbe.domain.problem.dto.response.ProblemHintResponseDto;
 import com.syu.capsbe.domain.problem.dto.response.ProblemResponseDto;
 import java.util.List;
 
 public interface ProblemService {
 
     List<ProblemResponseDto> getProblemsByProblemType(ProblemRequestDto problemRequestDto);
+
+    ProblemHintResponseDto getHintByQuestion(ProblemHintRequestDto problemHintRequestDto);
 }
