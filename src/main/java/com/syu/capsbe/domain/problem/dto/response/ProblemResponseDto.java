81f132eca5717d9351c96c7da97d24475828da53
kimnamgyu <lethargic.hood@gmail.com>
2024-04-25 22:56:02 +0900

    docs #38: Problem 도메인 관련 swagger 문서화

--- src/main/java/com/syu/capsbe/domain/problem/dto/response/ProblemResponseDto.java
+++ src/main/java/com/syu/capsbe/domain/problem/dto/response/ProblemResponseDto.java
@@ -1,18 +1,23 @@
 package com.syu.capsbe.domain.problem.dto.response;
 
 import com.syu.capsbe.domain.problem.Problem;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Schema(description = "문제 응답 정보")
 public class ProblemResponseDto {
 
+    @Schema(description = "문제 유형", example = "WORD | CONVERSATION | GRAMMAR")
     private final String problemType;
 
+    @Schema(description = "문제", example = "Could you please send me the report by 3 PM today? Sure, I'll ______ it to you before then. Options: a) email, b) describe, c) announce, d) discuss")
     private final String question;
 
+    @Schema(description = "정답", example = "a) email")
     private final String answer;
 
     public static ProblemResponseDto of(Problem problem) {
