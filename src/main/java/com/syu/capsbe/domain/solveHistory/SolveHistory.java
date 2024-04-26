66ef3d8e175c6c2bf03e03449ca0dc9c37129dcc
kimnamgyu <lethargic.hood@gmail.com>
2024-04-27 01:49:54 +0900

    refactor #45: 사용하지 않는 import 제거

--- src/main/java/com/syu/capsbe/domain/solveHistory/SolveHistory.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/SolveHistory.java
@@ -1,7 +1,6 @@
 package com.syu.capsbe.domain.solveHistory;
 
 import com.syu.capsbe.domain.member.Member;
-import com.syu.capsbe.domain.problem.Problem;
 import com.syu.capsbe.domain.problem.ProblemType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -11,7 +10,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -19,7 +17,6 @@ import java.util.List;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
 @Entity
@@ -44,16 +41,17 @@ public class SolveHistory {
 
     private LocalDateTime solveDate;
 
-    public void addSolveHistoryDetail(SolveHistoryDetail solveHistoryDetail) {
-        this.solveHistoryDetail.add(solveHistoryDetail);
-        solveHistoryDetail.setSolveHistory(this);
-    }
-
-    public SolveHistory(Member member,Long solveCount ,ProblemType problemType, LocalDateTime solveDate, List<SolveHistoryDetail> solveHistoryDetail) {
+    public SolveHistory(Member member, Long solveCount, ProblemType problemType,
+            LocalDateTime solveDate, List<SolveHistoryDetail> solveHistoryDetail) {
         this.member = member;
         this.solveCount = solveCount;
         this.problemType = problemType;
         this.solveDate = solveDate;
         this.solveHistoryDetail = solveHistoryDetail;
+    }
+
+    public void addSolveHistoryDetail(SolveHistoryDetail solveHistoryDetail) {
+        this.solveHistoryDetail.add(solveHistoryDetail);
+        solveHistoryDetail.setSolveHistory(this);
     }
 }
