26750093c747d46061627b01a2a84b9820fca4ae
kimnamgyu <lethargic.hood@gmail.com>
2024-04-27 01:25:22 +0900

    docs #45: 요청 응답 정보 문서화

--- src/main/java/com/syu/capsbe/domain/solveHistory/dto/request/SolveHistoryRequestDto.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/dto/request/SolveHistoryRequestDto.java
@@ -10,7 +10,51 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SolveHistoryRequestDto {
 
+    @Schema(description = "문제 유형", example = "GRAMMAR | CONVERSATION | WORD")
     private String problemType;
 
+    @Schema(description = "풀이 세부 기록 리스트", example = "[\n"
+            + "                {\n"
+            + "                    \"question\": \"What is the best synonym for 'enduring' in the context of business relationships? Choices: a) lasting, b) brief, c) fragile, d) sporadic\",\n"
+            + "                    \"answer\": \"a\",\n"
+            + "                    \"userAnswer\": \"b\",\n"
+            + "                    \"correct\": false\n"
+            + "                },\n"
+            + "                {\n"
+            + "                    \"question\": \"Identify the error in the sentence: 'She has less clients than she did last year.' Choices: a) She has, b) less, c) than she, d) last year\",\n"
+            + "                    \"answer\": \"b\",\n"
+            + "                    \"userAnswer\": \"b\",\n"
+            + "                    \"correct\": true\n"
+            + "                },\n"
+            + "                {\n"
+            + "                    \"question\": \"What does the phrase 'touch base' mean in a business context? Choices: a) To negotiate, b) To update, c) To argue, d) To finalize\",\n"
+            + "                    \"answer\": \"b\",\n"
+            + "                    \"userAnswer\": \"b\",\n"
+            + "                    \"correct\": true\n"
+            + "                }\n"
+            + "            ]\n"
+            + "        },\n"
+            + "        {\n"
+            + "            \"solveHistoryCount\": 2,\n"
+            + "            \"solveHistoryDetail\": [\n"
+            + "                {\n"
+            + "                    \"question\": \"What is the best synonym for 'enduring' in the context of business relationships? Choices: a) lasting, b) brief, c) fragile, d) sporadic\",\n"
+            + "                    \"answer\": \"a\",\n"
+            + "                    \"userAnswer\": \"a\",\n"
+            + "                    \"correct\": true\n"
+            + "                },\n"
+            + "                {\n"
+            + "                    \"question\": \"Identify the error in the sentence: 'She has less clients than she did last year.' Choices: a) She has, b) less, c) than she, d) last year\",\n"
+            + "                    \"answer\": \"b\",\n"
+            + "                    \"userAnswer\": \"b\",\n"
+            + "                    \"correct\": true\n"
+            + "                },\n"
+            + "                {\n"
+            + "                    \"question\": \"What does the phrase 'touch base' mean in a business context? Choices: a) To negotiate, b) To update, c) To argue, d) To finalize\",\n"
+            + "                    \"answer\": \"b\",\n"
+            + "                    \"userAnswer\": \"b\",\n"
+            + "                    \"correct\": true\n"
+            + "                }\n"
+            + "            ]")
     private List<SolveHistoryDetailRequestDto> solveHistoryDetail;
 }
