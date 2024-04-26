dd124b472470f1a751f626b030d29d55220466b9
kimnamgyu <lethargic.hood@gmail.com>
2024-04-27 01:25:22 +0900

    docs #45: 요청 응답 정보 문서화

--- src/main/java/com/syu/capsbe/domain/solveHistory/dto/request/SolveHistoryDetailRequestDto.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/dto/request/SolveHistoryDetailRequestDto.java
@@ -9,11 +9,15 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SolveHistoryDetailRequestDto {
 
+    @Schema(description = "문제", example = "Fill in the blank: 'Due to unforeseen circumstances, the meeting has been _______.' Choices: a) canceled, b) advanced, c) delayed, d) extended")
     private String question;
 
+    @Schema(description = "정답", example = "c")
     private String answer;
 
+    @Schema(description = "유저 제출 정답", example = "c")
     private String userAnswer;
 
+    @Schema(description = "정답 여부", example = "true")
     private boolean isCorrect;
 }
--- src/main/java/com/syu/capsbe/domain/solveHistory/dto/response/SolveHistoryResponseDto.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/dto/response/SolveHistoryResponseDto.java
@@ -1,20 +1,29 @@
 package com.syu.capsbe.domain.solveHistory.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.ArrayList;
 import java.util.List;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Schema(description = "문제 풀이 전체 기록 응답 정보")
 public class SolveHistoryResponseDto {
 
+    @Schema(description = "풀이 기록 번호 | 문제 리스트 번호", example = "3")
     private Long solveHistoryCount;
 
+    @Schema(description = "풀이 세부 기록", example = "3")
     private List<SolveHistoryDetailResponse> solveHistoryDetail = new ArrayList<>();
 
-    @Builder
-    public SolveHistoryResponseDto(Long solveHistoryCount, List<SolveHistoryDetailResponse> solveHistoryDetail) {
-        this.solveHistoryCount = solveHistoryCount;
-        this.solveHistoryDetail = solveHistoryDetail;
+    public static SolveHistoryResponseDto of(Long solveHistoryCount, List<SolveHistoryDetailResponse> solveHistoryDetail) {
+        return SolveHistoryResponseDto.builder()
+                .solveHistoryCount(solveHistoryCount)
+                .solveHistoryDetail(solveHistoryDetail)
+                .build();
     }
 }
