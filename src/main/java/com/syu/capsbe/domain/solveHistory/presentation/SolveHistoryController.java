7402d806bcc0b57b3347835f4aeeab73f924c5cb
kimnamgyu <lethargic.hood@gmail.com>
2024-05-01 18:20:18 +0900

    feat #53: 문제 풀이 기록 제출 메서드 변경
    - 반환 타입 추가 SubmissionResponseDto
    - 매 문제마다 요청하고, 정답을 반환

--- src/main/java/com/syu/capsbe/domain/solveHistory/presentation/SolveHistoryController.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/presentation/SolveHistoryController.java
@@ -2,9 +2,10 @@ package com.syu.capsbe.domain.solveHistory.presentation;
 
 import com.syu.capsbe.domain.member.Member;
 import com.syu.capsbe.domain.solveHistory.application.SolveHistoryService;
-import com.syu.capsbe.domain.solveHistory.dto.request.SolveHistoryRequestDto;
+import com.syu.capsbe.domain.solveHistory.dto.request.SolveHistoryDetailRequestDto;
 import com.syu.capsbe.domain.solveHistory.dto.response.SolveHistoryDetailResponse;
 import com.syu.capsbe.domain.solveHistory.dto.response.SolveHistoryResponseDto;
+import com.syu.capsbe.domain.solveHistory.dto.response.SubmissionResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -29,9 +30,10 @@ public class SolveHistoryController {
     @PostMapping("/submit")
     @Operation(summary = "문제 풀이 기록 제출", description = "문제 풀이 기록을 제출합니다. 사용자는 모든 문제를 푼 후에 제출할 수 있습니다.")
     @ApiResponse(responseCode = "200", description = "문제 풀이 기록 제출 성공")
-    public void submitSolveHistory(@RequestBody SolveHistoryRequestDto request,
+    public SubmissionResponseDto submitSolveHistory(
+            @RequestBody SolveHistoryDetailRequestDto request,
             @AuthenticationPrincipal Member member) {
-        solveHistoryService.submitSolveHistory(request, member.getId());
+        return solveHistoryService.submitSolveHistory(request, member.getId());
     }
 
     @GetMapping("/list")
@@ -45,7 +47,8 @@ public class SolveHistoryController {
     @Operation(summary = "문제 풀이 기록 세부 조회", description = "사용자의 문제 풀이 기록 세부를 조회합니다. 문제 풀이 기록 번호를 통해 세부 조회가 가능합니다.")
     @ApiResponse(responseCode = "200", description = "문제 풀이 기록 세부 조회 성공")
     @ApiResponse(responseCode = "E2001", description = "해당 문제 풀이 기록이 존재하지 않습니다.")
-    public List<SolveHistoryDetailResponse> getHistoryDetails(@AuthenticationPrincipal Member member,
+    public List<SolveHistoryDetailResponse> getHistoryDetails(
+            @AuthenticationPrincipal Member member,
             @PathVariable Long solveCount) {
         return solveHistoryService.getHistoryDetails(member.getId(), solveCount);
     }
