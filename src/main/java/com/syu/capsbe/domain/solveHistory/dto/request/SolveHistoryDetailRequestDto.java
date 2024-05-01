4dd3cfadab2211275fa021fa11b69e45767b35d5
kimnamgyu <lethargic.hood@gmail.com>
2024-05-01 15:45:19 +0900

    mod #53: 클라이언트 문제 기록 요청 정보 변경
    - 문제 번호, 제출 정답

--- src/main/java/com/syu/capsbe/domain/solveHistory/dto/request/SolveHistoryDetailRequestDto.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/dto/request/SolveHistoryDetailRequestDto.java
@@ -11,11 +11,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public class SolveHistoryDetailRequestDto {
 
-    @Schema(description = "문제", example = "Fill in the blank: 'Due to unforeseen circumstances, the meeting has been _______.' Choices: a) canceled, b) advanced, c) delayed, d) extended")
-    private String question;
-
-    @Schema(description = "정답", example = "c")
-    private String answer;
+    @Schema(description = "문제 번호", example = "1")
+    private Long problemId;
 
     @Schema(description = "유저 제출 정답", example = "c")
     private String userAnswer;
@@ -23,8 +20,6 @@ public class SolveHistoryDetailRequestDto {
     public SolveHistoryDetail toEntity(SolveHistory solveHistory) {
         return SolveHistoryDetail.builder()
                 .solveHistory(solveHistory)
-                .question(question)
-                .answer(answer)
                 .userAnswer(userAnswer)
                 .build();
     }
