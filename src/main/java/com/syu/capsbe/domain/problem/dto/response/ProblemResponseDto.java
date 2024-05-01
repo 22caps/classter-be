d0e2abd38a0ef115639ffc8dd7e14e63d55588eb
kimnamgyu <lethargic.hood@gmail.com>
2024-05-01 14:51:25 +0900

    feat #52: 문제 응답 정보에 문제 번호 추가

--- src/main/java/com/syu/capsbe/domain/problem/dto/response/ProblemResponseDto.java
+++ src/main/java/com/syu/capsbe/domain/problem/dto/response/ProblemResponseDto.java
@@ -11,17 +11,20 @@ import lombok.RequiredArgsConstructor;
 @Schema(description = "문제 응답 정보")
 public class ProblemResponseDto {
 
+    @Schema(description = "문제 번호", example = "1")
+    private final Long problemId;
+
     @Schema(description = "문제 유형", example = "WORD | CONVERSATION | GRAMMAR")
     private final String problemType;
 
     @Schema(description = "문제", example = "Could you please send me the report by 3 PM today? Sure, I'll ______ it to you before then. Options: a) email, b) describe, c) announce, d) discuss")
     private final String question;
 
-    @Schema(description = "정답", example = "a) email")
+    @Schema(description = "정답", example = "a")
     private final String answer;
 
     public static ProblemResponseDto of(Problem problem) {
-        return new ProblemResponseDto(problem.getProblemType().name(), problem.getQuestion(),
+        return new ProblemResponseDto(problem.getId(), problem.getProblemType().name(), problem.getQuestion(),
                 problem.getAnswer());
     }
 }
