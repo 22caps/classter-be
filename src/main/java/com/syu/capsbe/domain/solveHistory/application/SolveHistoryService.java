dcd247e375c4a58b5fa6c95952819b5c82cb4a3b
kimnamgyu <lethargic.hood@gmail.com>
2024-05-08 16:56:38 +0900

    mod #67: 사용하지 않는 메서드 제거 및 solveHistory setUp 메서드 구현

--- src/main/java/com/syu/capsbe/domain/solveHistory/application/SolveHistoryService.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/application/SolveHistoryService.java
@@ -1,18 +1,19 @@
 package com.syu.capsbe.domain.solveHistory.application;
 
-import com.syu.capsbe.domain.member.Member;
-import com.syu.capsbe.domain.problem.ProblemType;
 import com.syu.capsbe.domain.solveHistory.dto.request.SolveHistoryDetailRequestDto;
+import com.syu.capsbe.domain.solveHistory.dto.request.SolveHistorySetUpRequestDto;
 import com.syu.capsbe.domain.solveHistory.dto.response.SolveHistoryDetailResponse;
 import com.syu.capsbe.domain.solveHistory.dto.response.SolveHistoryResponseDto;
+import com.syu.capsbe.domain.solveHistory.dto.response.SolveHistorySetUpResponseDto;
 import com.syu.capsbe.domain.solveHistory.dto.response.SubmissionResponseDto;
 import java.util.List;
 
 public interface SolveHistoryService {
 
-    SubmissionResponseDto submitSolveHistory(SolveHistoryDetailRequestDto request, Long memberId);
+    SolveHistorySetUpResponseDto setSolveHistory(SolveHistorySetUpRequestDto request,
+            Long memberId);
 
-    void setSolveHistory(Member member, Long solveCount, ProblemType problemType);
+    SubmissionResponseDto submitSolveHistory(SolveHistoryDetailRequestDto request, Long memberId);
 
     List<SolveHistoryResponseDto> getHistoryList(Long memberId);
 
