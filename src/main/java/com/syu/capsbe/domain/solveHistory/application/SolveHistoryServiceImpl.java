25718643923a5791f755f13fc440c1cd42db60d8
kimnamgyu <lethargic.hood@gmail.com>
2024-05-19 13:39:53 +0900

    feat #75: 예외 처리를 위한 로직 추가

--- src/main/java/com/syu/capsbe/domain/solveHistory/application/SolveHistoryServiceImpl.java
+++ src/main/java/com/syu/capsbe/domain/solveHistory/application/SolveHistoryServiceImpl.java
@@ -45,7 +45,7 @@ public class SolveHistoryServiceImpl implements SolveHistoryService {
 
         SolveHistory solveHistory = solveHistoryRepository.save(
                 new SolveHistory(member, problemType, LocalDateTime.now())
-        );
+        ); // SolveHistory 생성
 
         return SolveHistorySetUpResponseDto.of(solveHistory.getId());
     }
@@ -63,7 +63,7 @@ public class SolveHistoryServiceImpl implements SolveHistoryService {
 
         solveHistoryDetailRepository.save(
                 new SolveHistoryDetail(solveHistory, problem, request.getUserAnswer(), isCorrect)
-        );
+        ); // SolveHistoryDetail 생성
 
         checkSolverHistoryComplete(solveHistory, request.getProblemNumber(), request.getLastProblemNumber());
 
@@ -75,6 +75,11 @@ public class SolveHistoryServiceImpl implements SolveHistoryService {
     }
 
     private void checkSolverHistoryComplete(SolveHistory solveHistory, int problemNumber, int lastProblemNumber) {
+        // TODO: validate solveHistory
+
+        if (solveHistory.getSolveHistoryDetailsSize() > lastProblemNumber)
+            throw new IllegalArgumentException("문제 풀이가 이미 완료됐어야 하는 상태입니다.");
+
         if (problemNumber == lastProblemNumber) solveHistory.completeSolveHistory();
     }
 
