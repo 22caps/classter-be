0ffffffec6a0889c9f85b89663be4bb6b5f7a937
kimnamgyu <lethargic.hood@gmail.com>
2024-10-23 03:45:00 +0900

    feat: auth0 적용

--- src/main/java/com/syu/capsbe/domain/auth/presentation/Auth0Controller.java
+++ src/main/java/com/syu/capsbe/domain/auth/presentation/Auth0Controller.java
@@ -6,22 +6,18 @@ import org.springframework.security.oauth2.client.registration.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.*;
 import java.net.URI;
+import reactor.core.publisher.Mono;
 
 @Controller
 public class Auth0Controller {
 
     @Value("${auth0.domain}")
     private String auth0Domain;
-
-    @Value("${auth0.clientId}")
-    private String clientId;
-
-    @Value("${auth0.callbackUrl}")
-    private String redirectUri;
 
     @GetMapping("/auth")
     public void authProxy(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -41,25 +37,18 @@ public class Auth0Controller {
     public ResponseEntity<String> tokenProxy(HttpServletRequest request) throws Exception {
         String tokenEndpoint = "https://" + auth0Domain + "/oauth/token";
 
-        // 요청의 form 데이터를 그대로 전달
-        RestTemplate restTemplate = new RestTemplate();
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
-
         // 요청 파라미터 가져오기
         String formData = request.getReader().lines()
                 .reduce("", (accumulator, actual) -> accumulator + actual);
 
-        HttpEntity<String> entity = new HttpEntity<>(formData, headers);
+        WebClient webClient = WebClient.create();
+        Mono<String> response = webClient.post()
+                .uri(tokenEndpoint)
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .bodyValue(formData)
+                .retrieve()
+                .bodyToMono(String.class);
 
-        ResponseEntity<String> response = restTemplate.exchange(
-                tokenEndpoint,
-                HttpMethod.POST,
-                entity,
-                String.class
-        );
-
-        return ResponseEntity.status(response.getStatusCode())
-                .body(response.getBody());
+        return ResponseEntity.ok(response.block());
     }
 }
